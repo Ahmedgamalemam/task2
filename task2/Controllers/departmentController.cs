76aa9aab602b27417d2106779b458c0fbffdd889
GEMY <GEMY@DESKTOP-GFUDS8G>
2023-02-02 21:25:06 +0200

    task5 and 6

--- task2/Controllers/departmentController.cs
+++ task2/Controllers/departmentController.cs
@@ -17,5 +17,51 @@ namespace task2.Controllers
             else
                 return View("get_department",a);
         }
+        public IActionResult allDepartments()
+        {
+            var a = DB.Departments.ToList();
+            return View(a);
+        }
+        public IActionResult details(int id)
+        {
+            var a = DB.Departments.Where(x => x.Dnum == id).SingleOrDefault();
+
+            return View(a);
+        }
+        public IActionResult addform()
+        {
+            return View();
+        }
+        public IActionResult add(department d)
+        {
+            DB.Departments.Add(d);
+            DB.SaveChanges();
+
+            return RedirectToAction(nameof(allDepartments));
+        }
+        public IActionResult editform(int id)
+        {
+            var a = DB.Departments.Where(x => x.Dnum == id).SingleOrDefault();
+
+
+            return View(a);
+        }
+        public IActionResult edit(department d)
+        {
+            var a = DB.Departments.Where(x => x.Dnum == d.Dnum).SingleOrDefault();
+
+            a.DName = d.DName;
+            DB.SaveChanges();
+
+            return RedirectToAction(nameof(allDepartments));
+        }
+        public IActionResult delete(int id)
+        {
+            var a = DB.Departments.SingleOrDefault(d => d.Dnum == id);
+            DB.Departments.Remove(a);
+            DB.SaveChanges();
+            return RedirectToAction(nameof(allDepartments));
+
+        }
     }
 }
